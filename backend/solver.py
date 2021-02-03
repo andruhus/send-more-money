bb4a649aa52e21c1fefa7442db42cb811464d58a
andruhus <aaademed@gmail.com>
2021-02-03 20:16:24 +0200

    Initialized empty system of linear equations

--- backend/solver.py
+++ backend/solver.py
@@ -2,14 +2,22 @@ import numpy as np
 def check_combination(add1,add2,sum):
     return len(set(add1+add2+sum)) <= 10
 
+def empty_system_initiate(total_len):
+    matrix = np.zeros((total_len ** 2,total_len))
+    extra_column = np.zeros(total_len ** 2)
+    return matrix,extra_column
 
+def get_free_row_index(matrix):
+    for i in range(matrix.shape[1]):
+
+def set_equation_for_one_letter(matrix,):
+    pass
 
 def solve(add1,add2,sum):
     if check_combination(add1,add2,sum):
         total_len = len(add1) + len(add2) + len(sum)
-        matrix = np.zeros((total_len,total_len**2))
-        extra_column = np.zeros(total_len**2)
-        
+        matrix, extra_column = empty_system_initiate(total_len)
+
         pass
 
 solve('send','more','money')
