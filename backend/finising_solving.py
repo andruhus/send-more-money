dbab8a16408af0ad4640ebfd766cb86fa50b8258
andruhus <aaademed@gmail.com>
2021-02-18 19:06:23 +0200

    Finished get_all_results

--- backend/finising_solving.py
+++ backend/finising_solving.py
@@ -3,10 +3,16 @@ import numpy as np
 def get_all_results(matrix,extra_column,len1,len2,len3):
     results = []
     for shift in range(len3):
-        pass
+        temp_res = get_result_for_shift(shift,matrix,extra_column,len1,len2,len3)
+        if len(temp_res.keys()) > 0:
+            results.append(temp_res)
     return results
 
 def integer_to_binary_list(i):
     binary = np.binary_repr(i)
     return [int(x) for x in str(binary)]
+
+def get_result_for_shift(shift,matrix,extra_column,len1,len2,len3):
+    result = {}
+    return result
 
