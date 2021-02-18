e1121615760277cfce52f952dd758d181bebb3cb
andruhus <aaademed@gmail.com>
2021-02-18 18:58:29 +0200

    Implemented integer_to_binary_list

---
+++ backend/finising_solving.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def get_all_results(matrix,extra_column,len1,len2,len3):
+    results = []
+    for shift in range(len3):
+        pass
+    return results
+
+def integer_to_binary_list(i):
+    binary = np.binary_repr(i)
+    return [int(x) for x in str(binary)]
+
--- backend/source/finising_solving.py
+++
@@ -1,4 +0,0 @@
-
-def get_all_results(matrix,extra_column,len1,len2,len3):
-    results = []
-    return results
