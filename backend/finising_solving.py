52e18164102d422105e611b56044df24a9217ff8
andruhus <aaademed@gmail.com>
2021-05-02 18:51:56 +0300

    Finished solving

--- backend/finising_solving.py
+++ backend/finising_solving.py
@@ -2,22 +2,27 @@ import numpy as np
 import scipy.optimize as sco
 from letter_equations import get_free_row_index
 
+
 def get_all_results(matrix, extra_column, len1, len2, len3):
     results = []
     for shift in range(2 ** (len3 - 1)):
         temp_res = get_result_for_shift(shift, matrix, extra_column, len1, len2, len3)
+
         if not temp_res == None:
             results.append(temp_res)
     return results
 
 
-def integer_to_binary_list(i):
+def integer_to_binary_list(i, total):
     binary = np.binary_repr(i)
-    return [int(x) for x in str(binary)]
+    res = [int(x) for x in str(binary)]
+    while len(res) < total:
+        res.insert(0, 0)
+    return res
 
 
 def get_result_for_shift(shift, matrix, extra_column, len1, len2, len3):
-    shift_list = integer_to_binary_list(shift)
+    shift_list = integer_to_binary_list(shift, len3 - 1)
     adding_equation(shift_list, matrix, extra_column, len1, len2, len3)
     solution = solve_sys_lin_equations(matrix, extra_column)
     if analyze_solution(solution):
@@ -26,16 +31,20 @@ def get_result_for_shift(shift, matrix, extra_column, len1, len2, len3):
 
 def adding_equation(shift_list, matrix, extra_column, len1, len2, len3):
     for i in range(len3):
-        list_of_indeces = get_indeces_list(i,len1,len2,len3)
+        list_of_indeces = get_indeces_list(i, len1, len2, len3)
         free_row = get_free_row_index(matrix)
-        if i != 0:
-            matrix[free_row],extra_column[free_row] = create_row(shift_list[i-1],shift_list[i],list_of_indeces,len1+len2+len3)
+        if i == 0:
+            matrix[free_row], extra_column[free_row] = create_row(0, shift_list[i], list_of_indeces, len1 + len2 + len3)
+
+        elif i == len3 - 1:
+            matrix[free_row], extra_column[free_row] = create_row(shift_list[i - 1], 0, list_of_indeces,
+                                                                  len1 + len2 + len3)
         else:
-            matrix[free_row],extra_column[free_row] = create_row(0,shift_list[i],list_of_indeces,len1+len2+len3)
+            matrix[free_row], extra_column[free_row] = create_row(shift_list[i - 1], shift_list[i], list_of_indeces,
+                                                                  len1 + len2 + len3)
 
 
-
-def create_row(prev_shift,next_shift,list_of_indeces,total):
+def create_row(prev_shift, next_shift, list_of_indeces, total):
     row = np.zeros(total)
     if not list_of_indeces[0] == None:
         row[list_of_indeces[0]] = 1
@@ -43,9 +52,10 @@ def create_row(prev_shift,next_shift,list_of_indeces,total):
         row[list_of_indeces[1]] = 1
     row[list_of_indeces[2]] = -1
     extr_col = 10 * next_shift - prev_shift
-    return row,extr_col
+    return row, extr_col
 
-def get_indeces_list(i,len1,len2,len3):
+
+def get_indeces_list(i, len1, len2, len3):
     total = len3 + len2 + len1
     ind_sum = total - 1 - i
     ind_add1 = None
@@ -54,7 +64,8 @@ def get_indeces_list(i,len1,len2,len3):
         ind_add1 = total - 1 - i - len3 - len2
     if i < len2:
         ind_add2 = total - 1 - i - len3
-    return [ind_add1,ind_add2,ind_sum]
+    return [ind_add1, ind_add2, ind_sum]
+
 
 def analyze_solution(solution):
     if solution == None:
@@ -68,11 +79,18 @@ def analyze_solution(solution):
 
 def solve_sys_lin_equations(matrix, extra_column):
     extended_matr = np.concatenate((matrix, extra_column), axis=1)
+
     extended_matr = sco._remove_redundancy._remove_redundancy(extended_matr, np.zeros_like(extended_matr[:, 0]))[0]
+
+    print(extended_matr)
+
     temp_list = np.array_split(extended_matr, extended_matr.shape[1] - 1, axis=1)
     matrix, extra_column = temp_list[0], temp_list[1]
     if check_sys_lin_equation(matrix):
         return np.linalg.solve(matrix, extra_column)
+    else:
+        # todo recursive func
+        pass
 
 
 def check_sys_lin_equation(matrix):
--- backend/solver.py
+++ backend/solver.py
@@ -4,9 +4,9 @@ from finising_solving import get_all_results
 def check_combination(add1,add2,sum):
     return len(set(add1+add2+sum)) <= 10
 
-def empty_system_initiate(total_len):
-    matrix = np.zeros((total_len ** 2,total_len))
-    extra_column = np.zeros(total_len ** 2)
+def empty_system_initiate(total_len,len3):
+    matrix = np.zeros((total_len + len3 + 1,total_len))
+    extra_column = np.zeros((total_len + len3 + 1,1))
     return matrix,extra_column
 
 def create_dict_of_letters(str):
@@ -20,12 +20,15 @@ def create_dict_of_letters(str):
 def solve(add1,add2,sum):
     result = {}
     if check_combination(add1,add2,sum):
+
         total_len = len(add1) + len(add2) + len(sum)
-        matrix, extra_column = empty_system_initiate(total_len)
+        matrix, extra_column = empty_system_initiate(total_len,len(sum))
         lett_dict = create_dict_of_letters(add1+add2+sum)
         set_equation_for_all_letter(matrix,lett_dict)
         solution_array = get_all_results(matrix,extra_column,len(add1),len(add2),len(sum))
-        result = represent_to_dict(add1,add2,sum,solution_array)
+        if len(solution_array) == 1:
+            result = represent_to_dict(add1,add2,sum,solution_array[0])
+
     return result
 
 def represent_to_dict(add1,add2,sum,solution_array):
@@ -35,4 +38,4 @@ def represent_to_dict(add1,add2,sum,solution_array):
         dict[concatenation[i]] = solution_array[i]
     return dict
 
-solve('send','more','money')
+print(solve('send','more','money'))
