f98744c3a59b0d1e841221e6aef4a4a0b3189867
IhorTarkhan <ihor.lafox.net>
2021-05-08 22:40:27 +0300

    create word view

--- frontend/src/page/InteractivePage.tsx
+++ frontend/src/page/InteractivePage.tsx
@@ -3,21 +3,51 @@ import { useParams } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { NavBar } from "../component/NavBar";
+import { QuestionTaskResponse } from "../dto/response/QuestionTaskResponse";
+import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  root: {
+  word: {
     color: "blue",
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  cell: {
+    flexGrow: 1,
+    maxWidth: "56px",
+    maxHeight: "56px",
+    margin: "5.6px",
   },
 }));
 
 export const InteractivePage = (): ReactElement => {
   const classes = useStyles();
   const { questionId } = useParams();
+  const task: QuestionTaskResponse = {
+    addition1: "send",
+    addition2: "more",
+    sum: "money",
+  };
 
   return (
     <>
       <NavBar />
-      <div className={classes.root}>Interactive page to task #{questionId}</div>
+      <div>ToDo get task by id: {questionId}</div>
+      <div className={classes.word}>
+        {task.addition1.split("").map((it) => (
+          <TextField className={classes.cell} label={it} variant="outlined" />
+        ))}
+      </div>
+      <div className={classes.word}>
+        {task.addition2.split("").map((it) => (
+          <TextField className={classes.cell} label={it} variant="outlined" />
+        ))}
+      </div>
+      <div className={classes.word}>
+        {task.sum.split("").map((it) => (
+          <TextField className={classes.cell} label={it} variant="outlined" />
+        ))}
+      </div>
     </>
   );
 };
