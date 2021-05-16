4eea7560e300e5a618e00b5f80f7b5e893598d1f
andruhus <aaademed@gmail.com>
2021-05-16 20:10:05 +0300

    Removed NotBad

--- API.py
+++ API.py
@@ -9,8 +9,6 @@ app.config["DEBUG"] = True
 @app.route('/questions-info', methods=['GET'])
 def get_all_tasks():
     beautiful = session.query(BeautifulSolution).all()
-    notbad = session.query(NotbadSolution).all()
-    beautiful.extend(notbad)
     return jsonify(beautiful)
 
 
@@ -21,54 +19,34 @@ def check_user_result(task_id):
     numbers = request.args.getlist('value')
     for index in range(len(letters)):
         user_result[letters[index]] = numbers[index]
-    if task_id >= session.query(BeautifulSolution).count():
-        obj = session.query(NotbadSolution).filter_by(id=task_id).first()
-        if obj.solution1 == user_result or obj.solution2 == user_result or obj.solution3 == user_result or obj.solution4 == user_result or obj.solution5 == user_result:
-            session.query(NotbadSolution).filter_by(id=task_id).first().update(
-                {NotbadSolution.solvedCount: NotbadSolution.solvedCount + 1})
-            return 200
-        else:
-            return 400
+    obj = session.query(BeautifulSolution).filter_by(id=task_id).first()
+    if obj.solution1 == user_result:
+        session.query(BeautifulSolution).filter_by(id=task_id).first().update(
+            {BeautifulSolution.solvedCount: BeautifulSolution.solvedCount + 1})
+        return 200
     else:
-        obj = session.query(BeautifulSolution).filter_by(id=task_id).first()
-        if obj.solution1 == user_result:
-            session.query(BeautifulSolution).filter_by(id=task_id).first().update(
-                {BeautifulSolution.solvedCount: BeautifulSolution.solvedCount + 1})
-            return 200
-        else:
-            return 400
+        return 400
 
 
 @app.route('/try-questions/<int:task_id>', methods=['POST'])
 def increase_solving_attempts(task_id):
-    if task_id < session.query(BeautifulSolution).count():
-        session.query(BeautifulSolution).filter_by(id=task_id).first().update(
-            {BeautifulSolution.triedCount: BeautifulSolution.triedCount + 1})
-    else:
-        session.query(NotbadSolution).filter_by(id=task_id).first().update(
-            {NotbadSolution.triedCount: NotbadSolution.triedCount + 1})
+    session.query(BeautifulSolution).filter_by(id=task_id).first().update(
+        {BeautifulSolution.triedCount: BeautifulSolution.triedCount + 1})
+
 
 
 @app.route('/like-questions/<int:task_id>', methods=['POST'])
 def increase_likes(task_id):
-    if task_id < session.query(BeautifulSolution).count():
-        session.query(BeautifulSolution).filter_by(id=task_id).first().update(
-            {BeautifulSolution.likeCount: BeautifulSolution.likeCount + 1})
-    else:
-        session.query(NotbadSolution).filter_by(id=task_id).first().update(
-            {NotbadSolution.likeCount: NotbadSolution.likeCount + 1})
+    session.query(BeautifulSolution).filter_by(id=task_id).first().update(
+        {BeautifulSolution.likeCount: BeautifulSolution.likeCount + 1})
 
 
 @app.route('/questions-info/<int:task_id>', methods=['GET'])
 def get_task_name(task_id):
-    if task_id < session.query(BeautifulSolution).count():
-        obj = session.query(BeautifulSolution).filter_by(id=task_id).first()
-        result = {'addition1': obj.add1, 'addition2': obj.add2, 'sum': obj.sum}
-        return jsonify(result)
-    else:
-        obj = session.query(NotbadSolution).filter_by(id=task_id).first()
-        result = {'addition1': obj.add1, 'addition2': obj.add2, 'sum': obj.sum}
-        return jsonify(result)
+    obj = session.query(BeautifulSolution).filter_by(id=task_id).first()
+    result = {'addition1': obj.add1, 'addition2': obj.add2, 'sum': obj.sum}
+    return jsonify(result)
+
 
 
 # todo for Ihor: you should manage links in paths
--- data.py
+++ data.py
@@ -22,28 +22,4 @@ class BeautifulSolution(Base):
     solvedCount = Column(Integer)
 
 
-class NotbadSolution(BeautifulSolution):
-    __tablename__ = 'notbad'
-    solution2 = Column(PickleType)
-    solution3 = Column(PickleType)
-    solution4 = Column(PickleType)
-    solution5 = Column(PickleType)
 
-    def __init__(self,solutions):
-        self.solution1 = solutions[0]
-        try:
-            self.solution2 = solutions[1]
-            try:
-                self.solution3 = solutions[2]
-                try:
-                    self.solution4 = solutions[3]
-                    try:
-                        self.solution5 = solutions[4]
-                    except:
-                        pass
-                except:
-                    pass
-            except:
-                pass
-        except:
-            pass
