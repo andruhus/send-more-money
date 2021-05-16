58dc6e3c43ce34ceffc7a713d3f729e22282de93
andruhus <aaademed@gmail.com>
2021-05-16 17:08:26 +0300

    Finished TR_33

--- API.py
+++ API.py
@@ -20,12 +20,19 @@ class ListView(Resource):
 
 
 class InterActive(Resource):
-    pass
+    def post(self,task_id,user_result):
+        if task_id >= session.query(BeautifulSolution).count():
+            obj = session.query(NotbadSolution).filter_by(id = task_id).first()
+            if obj.solution1 == user_result or obj.solution2 == user_result or obj.solution3 == user_result or obj.solution4 == user_result or obj.solution5 == user_result:
+                session.query(NotbadSolution).filter_by(id=task_id).first().update({NotbadSolution.solvedCount: NotbadSolution.solvedCount + 1 })
+                return 200
+            else:
+                return 400
 
 
 # todo for Ihor: you should manage links in paths
 api.add_resource(Home, '/')
 api.add_resource(ListView, '/questions-info/')
-api.add_resource(InterActive, '/interactive/<int: id>')
+api.add_resource(InterActive, '/solution-questions/<int:id>')
 
 app.run()
