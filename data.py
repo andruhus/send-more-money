382506262788222c01cb09c5115df93895d19b0a
andruhus <aaademed@gmail.com>
2021-05-16 12:56:01 +0300

    Created a structure of the database

---
+++ data.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column,Integer,String, PickleType
+engine = create_engine('sqlite:///solutions.db')
+session = sessionmaker(bind = engine)()
+
+Base = declarative_base()
+
+class BeautifulSolution(Base):
+
+    __tablename__ = 'beautiful'
+
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    add1 = Column(String)
+    add2 = Column(String)
+    sum = Column(String)
+    solution = Column(PickleType)
+    likeCount = Column(Integer)
+    triedCount = Column(Integer)
+    solvedCount = Column(Integer)
+
+
