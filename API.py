c88d8164aca5b538c9ccd3915cb8e1c9d6548399
andruhus <aaademed@gmail.com>
2021-05-16 16:47:22 +0300

    Finished TR_30

--- API.py
+++ API.py
@@ -1,13 +1,31 @@
-import flask
+from flask import Flask
+from flask_restful import Resource, Api
+from data import *
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
+api = Api(app)
 
-@app.route('/')
-class Home:
+
+class Home(Resource):
     pass
 
 
+class ListView(Resource):
+    def get(self):
+        beautiful = session.query(BeautifulSolution).all()
+        notbad = session.query(NotbadSolution).all()
+        beautiful.extend(notbad)
+        return beautiful
 
+
+class InterActive(Resource):
+    pass
+
+
+# todo for Ihor: you should manage links in paths
+api.add_resource(Home, '/')
+api.add_resource(ListView, '/questions-info/')
+api.add_resource(InterActive, '/interactive/<int: id>')
 
 app.run()
