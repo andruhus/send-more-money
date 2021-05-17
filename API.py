03e52d45244d6093a02710dc571719c6be59157e
andruhus <aaademed@gmail.com>
2021-05-17 22:50:55 +0300

    Fixed API

--- API.py
+++ API.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify, Response
+import json
 from data import *
 
 app = Flask(__name__)
@@ -8,42 +8,58 @@ app.config["DEBUG"] = True
 
 @app.route('/questions-info', methods=['GET'])
 def get_all_tasks():
+    session = sessionmaker(bind=engine)()
     beautiful = session.query(BeautifulSolution).all()
-    return jsonify(beautiful)
+    res = []
+    for item in beautiful:
+
+        res.append(item.to_dict())
+    return jsonify(res)
 
 
 @app.route('/solution-questions/<int:task_id>', methods=['POST'])
 def check_user_result(task_id):
+    session = sessionmaker(bind=engine)()
+    if task_id > session.query(BeautifulSolution).count():
+        return Response(status=404)
+    data = request.get_json()['data']
     user_result = {}
-    letters = request.args.getlist('char')
-    numbers = request.args.getlist('value')
-    for index in range(len(letters)):
-        user_result[letters[index]] = numbers[index]
+    for item in data:
+        user_result[item['char']] = item['value']
     obj = session.query(BeautifulSolution).filter_by(id=task_id).first()
     if obj.solution == user_result:
-        session.query(BeautifulSolution).filter_by(id=task_id).first().update(
+        session.query(BeautifulSolution).filter_by(id=task_id).update(
             {BeautifulSolution.solvedCount: BeautifulSolution.solvedCount + 1})
-        return 200
+        session.commit()
+        return Response(status=200)
     else:
-        return 400
+        return Response(status=400)
 
 
 @app.route('/try-questions/<int:task_id>', methods=['POST'])
 def increase_solving_attempts(task_id):
-    session.query(BeautifulSolution).filter_by(id=task_id).first().update(
+    session = sessionmaker(bind=engine)()
+    session.query(BeautifulSolution).filter_by(id=task_id).update(
         {BeautifulSolution.triedCount: BeautifulSolution.triedCount + 1})
+    session.commit()
+    return Response(status=200)
 
 
 
 @app.route('/like-questions/<int:task_id>', methods=['POST'])
 def increase_likes(task_id):
-    session.query(BeautifulSolution).filter_by(id=task_id).first().update(
+    session = sessionmaker(bind=engine)()
+    session.query(BeautifulSolution).filter_by(id=task_id).update(
         {BeautifulSolution.likeCount: BeautifulSolution.likeCount + 1})
+    session.commit()
+    return Response(status=200)
 
 
 @app.route('/questions-info/<int:task_id>', methods=['GET'])
 def get_task_name(task_id):
-    obj = session.query(BeautifulSolution).filter_by(id=task_id).first()
+    session = sessionmaker(bind=engine)()
+    obj = session.query(BeautifulSolution).filter_by(id = task_id).first()
+    print(obj)
     result = {'addition1': obj.add1, 'addition2': obj.add2, 'sum': obj.sum}
     return jsonify(result)
 
--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, PickleType
-
+from sqlalchemy import Column, Integer, String, PickleType, JSON
+import pickle
 engine = create_engine('sqlite:///solutions.db')
-session = sessionmaker(bind=engine)()
+
 
 Base = declarative_base()
 
@@ -12,12 +12,51 @@ Base = declarative_base()
 class BeautifulSolution(Base):
     __tablename__ = 'beautiful'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     add1 = Column(String)
     add2 = Column(String)
     sum = Column(String)
-    solution = Column(PickleType)
+    solution = Column(JSON)
     likeCount = Column(Integer)
     triedCount = Column(Integer)
     solvedCount = Column(Integer)
 
+    def __init__(self,add1,add2,sum,solution,likeCount,triedCount,solvedCount):
+        self.add1 = add1
+        self.add2 = add2
+        self.sum = sum
+        self.solution = solution
+        self.likeCount = likeCount
+        self.triedCount = triedCount
+        self.solvedCount = solvedCount
+
+    def to_dict(self):
+        return {
+            'add1':self.add1,
+            'add2':self.add2,
+            'sum':self.sum,
+            'solution':self.solution,
+            'likeCount':self.likeCount,
+            'triedCount':self.triedCount,
+            'solvedCount':self.solvedCount,
+                }
+
+# Base.metadata.create_all(engine)
+
+# with open('backend/source/soluving/words/beautiful.txt', 'r') as f:
+#     while True:
+#         try:
+#             s = f.readline()
+#             add1, add2, sum, solution = s.split(';')
+#             solution = eval(solution)
+#             # solution = PickleType(solution)
+#             session.add(BeautifulSolution(add1, add2, sum,solution,0,0,0))
+#         except:
+#             break
+
+
+
+# Base.metadata.create_all(engine)
+# a = session.query(BeautifulSolution).first().solution
+# print(a)
+# session.commit()
